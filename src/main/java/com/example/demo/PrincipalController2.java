d927592ff7e701a473cb3ca364f6231f6d6bee80
AnaJu <anajulya158@gmail.com>
2024-10-30 16:39:55 -0300

    atividades_30-10

---
+++ src/main/java/com/example/demo/PrincipalController2.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController2 {
+	
+	@GetMapping("/compras")
+	public String compras() {
+		return "compras";
+	}
+	
+	@GetMapping("/vendas")
+	public String vendas() {
+		return "vendas";
+	}
+	
+	@GetMapping("/estoque")
+	public String estoque() {
+		return "estoque";
+	}
+
+
+
+}
